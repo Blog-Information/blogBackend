4f4f9bcc8c8f83b362f3447d333c9fb5857f9cdf
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-17 17:58:59 +0800

    依赖文件

---
+++ src/main/java/com/steam_404/blogBacakend/BlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.steam_404.blogBacakend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steam_404/blogBacakend/BlogBcakendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steam_404.blogBacakend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBcakendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
