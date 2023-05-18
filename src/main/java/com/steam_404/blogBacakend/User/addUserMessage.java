0d25ad78a4bfe7db53b157cea80674ec11425f22
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-18 11:56:30 +0800

    添加用户留言

---
+++ src/main/java/com/steam_404/blogBacakend/User/addUserMessage.java
@@ -0,0 +1,37 @@
+//访客的数据
+package com.steam_404.blogBacakend.User;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.*;
+
+@RestController
+public class addUserMessage {
+    @CrossOrigin
+    @RequestMapping("/addUserMessage")
+//    访客添加留言功能
+    public void addMessage(
+            @RequestParam String timeStamp,
+            @RequestParam int year,
+            @RequestParam int month,
+            @RequestParam int day,
+            @RequestParam int hour,
+            @RequestParam int minute,
+            @RequestParam int second,
+            @RequestParam String content
+    ) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/blog";
+        String user = "root";
+        String password = "root";
+        String SQL = "insert into blog.messageboard(timeStamp, year, month, day, hour, minute, second, content)" +
+                " values ('" + timeStamp + "','" + year + "','" + month + "','" + day + "','" + hour + "','" + minute + "','" + second + "','" + content + "')";
+        Connection connection = DriverManager.getConnection(url, user, password);
+        Statement statement = connection.createStatement();
+        statement.execute(SQL);
+        statement.close();
+        connection.close();
+    }
+}
