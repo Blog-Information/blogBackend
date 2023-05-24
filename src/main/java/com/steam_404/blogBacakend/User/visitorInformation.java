6a228d03328a23aff50be51afa0d3167216e9324
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-24 20:07:36 +0800

    添加访客信息接口

---
+++ src/main/java/com/steam_404/blogBacakend/User/visitorInformation.java
@@ -0,0 +1,30 @@
+//访客信息
+package com.steam_404.blogBacakend.User;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.*;
+@RestController
+public class visitorInformation {
+    @CrossOrigin
+    @GetMapping(value = "visitorMessage")
+    public void visitorMessage(
+            @RequestParam String ipAddress,
+            @RequestParam String address,
+            @RequestParam String lsp,
+            @RequestParam String browser,
+            @RequestParam String system
+    )throws SQLException{
+        String url="jdbc:mysql://localhost:3306/blog";
+        String user="root";
+        String password="root";
+        String SQL="insert into access(data, ipAddress, address, lsp, browser, `system`)\n" +
+                "VALUES (now(),'"+ipAddress+"','"+address+"','"+lsp+"','"+browser+"','"+system+"')";
+        Connection connection=DriverManager.getConnection(url,user,password);
+        Statement statement=connection.createStatement();
+        statement.execute(SQL);
+    }
+}
